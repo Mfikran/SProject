9a21d4c0102e1f60ab1c7736739b77b092aa6593
Mfikran <mzulfikran@gmail.com>
2024-09-03 01:23:07 -0700

    configure firebase database

--- Contact Database(firebase)/Contact App/src/App.jsx
+++ Contact Database(firebase)/Contact App/src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div className="bg-red-500">App</div>; 
+  return <div>
+    <Navbar />
+  </div>; 
 };
 
 export default App;
---
+++ Contact Database(firebase)/Contact App/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+
+const Navbar = () => {
+    return( <div className="h-[6opx] bg-white m-4 rounded-lg">Navbar</div>
+    );
+};
+
+export default Navbar;
