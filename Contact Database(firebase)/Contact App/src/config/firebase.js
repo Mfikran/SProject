f7ed1b671189848e88a2d58e7d207005db61fad5
Mfikran <mzulfikran@gmail.com>
2024-09-02 22:48:16 -0700

    setup firebase
    setup firebase

---
+++ Contact Database(firebase)/Contact App/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_KJhr6gAxRnoXaR61WGFssw0MJiAY1ic",
+  authDomain: "phone-database-5e0b5.firebaseapp.com",
+  projectId: "phone-database-5e0b5",
+  storageBucket: "phone-database-5e0b5.appspot.com",
+  messagingSenderId: "1061218322192",
+  appId: "1:1061218322192:web:59753d08b71ce0bf9b0795"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
