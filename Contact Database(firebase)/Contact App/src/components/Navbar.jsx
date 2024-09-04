4fdd7a56125ea56f42d8735e3e428684e2485e04
Mfikran <mzulfikran@gmail.com>
2024-09-03 19:07:31 -0700

    Update Navbar.jsx

--- Contact Database(firebase)/Contact App/src/components/Navbar.jsx
+++ Contact Database(firebase)/Contact App/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-
 const Navbar = () => {
-    return( <div className="h-[6opx] bg-white m-4 rounded-lg">Navbar</div>
-    );
+  return (
+    <div className="h-[60px] bg-white m-4 gap-2 rounded-lg flex justify-center items-center text-xl font-medium">
+      <img src="/firebase.svg" alt="" />
+      <h1>Contact App</h1>
+    </div>
+  );
 };
 
 export default Navbar;
