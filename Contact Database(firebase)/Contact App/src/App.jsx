e07c9f775bf2e305d00c8766bf038944a3e00690
Mfikran <mzulfikran@gmail.com>
2024-09-03 20:49:02 -0700

    Create search bar

--- Contact Database(firebase)/Contact App/src/App.jsx
+++ Contact Database(firebase)/Contact App/src/App.jsx
@@ -1,8 +1,14 @@
 import Navbar from "./components/Navbar";
+import {FiSearch} from "react-icons/fi";
 
 const App = () => {
-  return <div>
+  return <div className="mx-auto max-w-[370px] px-4">
     <Navbar />
+    <div className="relative flex items-center">
+      <FiSearch className="absolute ml-1 text-3xl text-white"/>
+      <input type="text" className=" h-10 flex-grow border border-white rounded-md bg-transparent pl-9 text-white"/>
+    </div>
+
   </div>; 
 };
 
--- Contact Database(firebase)/Contact App/src/components/Navbar.jsx
+++ Contact Database(firebase)/Contact App/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="h-[60px] bg-white m-4 gap-2 rounded-lg flex justify-center items-center text-xl font-medium">
+    <div className="h-[60px] bg-white my-4 gap-2 rounded-lg flex justify-center items-center text-xl font-medium">
       <img src="/firebase.svg" alt="" />
       <h1>Contact App</h1>
     </div>
