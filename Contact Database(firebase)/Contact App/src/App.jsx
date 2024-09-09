d304ed349f29c87a0bcd7bf61397ba79097a1e9d
Mfikran <mzulfikran@gmail.com>
2024-09-08 19:23:24 -0700

    credential
    trying credential

--- Contact Database(firebase)/Contact App/src/App.jsx
+++ Contact Database(firebase)/Contact App/src/App.jsx
@@ -1,8 +1,30 @@
 import Navbar from "./components/Navbar";
 import { FiSearch } from "react-icons/fi";
 import { AiFillPlusCircle } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import {collection, getDoc, getDocs} from "firebase/firestore";
+import { db } from "./config/firebase";
 
 const App = () => {
+
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+
+    const getContacts = async () => {
+
+      try {
+
+        const contactsRef = collection (db, "contacts");
+        const contactsSnapshot = await getDocs(contactsRef);
+        const contactsLists = contactsSnapshot.docs.map((doc) => doc.data());
+        console.log(contactsLists);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getContacts();
+  }, []);
+
   return (
     <div className="mx-auto max-w-[370px] px-4">
       <Navbar />
